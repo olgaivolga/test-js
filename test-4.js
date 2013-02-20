f26ef72eb5bfb51a7ad30fe419799935e22e7250
olgaivolga <olgaivolga@gmail.com>
2013-02-20 17:29:37 +0100

    Rev 2

--- test-4.js
+++ test-4.js
@@ -1,5 +1,21 @@
-var clone = function(elementId) {
-	var target = elementId;
+var clone = function(element) {
+	var target = element;
 	var clone = target.cloneNode(true);
+	return clone;
+}
+
+var cloneRec = function(element) {
+	var target = element;
+	//console.log(clone);
+	if (clone == undefined) {
+		var clone = target.cloneNode(false); //???
+	}
+	var children = target.childNodes;
+	if (children != null) {
+		for (var i = 0; i < children.length; i++) {
+			cloneRec(clone.appendChild(children[i]));
+			console.log(clone);
+		}
+	}
 	return clone;
 }
