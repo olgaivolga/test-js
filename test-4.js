f20683cb4bde1c36fddd182051b692374aca97c7
olgaivolga <olgaivolga@gmail.com>
2013-02-20 20:09:04 +0100

    Rev 5

--- test-4.js
+++ test-4.js
@@ -5,13 +5,11 @@ var clone1 = function(element) {
 }
 
 var clone2 = function(element) {
-	var target = element;
-	var clone = target.parentNode; //???
-	var children = parent.childNodes;
+	var clone = element.cloneNode(false);
+	var children = element.childNodes;
 	if (children != null) {
 		for (var i = 0; i < children.length; i++) {
-			clone2(clone.appendChild(children[i]));
-			console.log(clone);
+			clone.appendChild(clone2(children[i]));
 		}
 	}
 	return clone;
