4d90991d3d80ae85dfecf820a251c534fe109413
olgaivolga <olgaivolga@gmail.com>
2013-02-21 20:31:00 +0100

    Rev 6

--- test-4.js
+++ test-4.js
@@ -14,3 +14,28 @@ var clone2 = function(element) {
 	}
 	return clone;
 }
+
+function clone3(element) {
+	var clone;
+	if(element.tagName) {
+		clone = document.createElement(element.tagName)
+	} else {
+		clone = document.createTextNode(element.textContent)
+	}
+	if (element.attributes != null) {
+		for (var i=0, attrs=element.attributes, l=attrs.length; i<l; i++){
+			//clone[attrs.item(i).nodeName] = attrs.item(i).value;
+			clone.setAttribute(attrs.item(i).nodeName, attrs.item(i).value) 
+			console.log(attrs.item(i).nodeName);
+		}
+	}
+	var children = element.childNodes;
+	if (children != null) {
+		for (var i = 0; i < children.length; i++) {
+			clone.appendChild(clone3(children[i]));
+		}
+	}
+	return clone;
+}
+
+
